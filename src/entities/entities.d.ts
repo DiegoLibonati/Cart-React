af2a04aa02e25734e88ac050286f98f4601f0319
Die <dlibonati@leafnoise.io>
2024-10-23 21:07:48 -0300

    patch: entities updated

--- src/entities/entities.d.ts
+++ src/entities/entities.d.ts
@@ -16,6 +16,8 @@ export type AppContextT = {
   clearItem: (id: number) => void;
   increaseItem: (id: number) => void;
   decreaseItem: (id: number) => void;
+  displayItems: (cart: Phone[]) => void;
+  setLoading: () => void;
 };
 
 export type Phone = {
@@ -26,17 +28,17 @@ export type Phone = {
   amount: number;
 };
 
-export type PayloadReducer = {
-  type: string;
-  payload?: any;
-};
+export type PayloadReducer =
+  | { type: "CLEAR_CART" }
+  | { type: "LOADING" }
+  | { type: "SET_TOTALS_AND_AMOUNT" }
+  | { type: "CLEAR_ITEM"; payload: { id: number } }
+  | { type: "INCREASE_ITEM"; payload: { id: number } }
+  | { type: "DECREASE_ITEM"; payload: { id: number } }
+  | { type: "DISPLAY_ITEMS"; payload: { cart: Phone[] } };
 
 // Interfaces
 
 export interface CartItemProps {
   id: number;
-  title: string;
-  price: number;
-  img: string;
-  amount: number;
 }
